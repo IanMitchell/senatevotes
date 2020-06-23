9847563e3820d8ee758a5a0dabfa6d054b212506
Ian Mitchell <ian.mitchel1@live.com>
2020-06-23 15:59:32 -0700

    Fix Color Picker

---
+++ components/ColorManager.js
@@ -0,0 +1,63 @@
+import MicroModal from 'micromodal';
+import React, { Fragment, useContext } from 'react';
+import ColorContext from '../contexts/ColorContext';
+import ColorPickerTrigger from './ColorPickerTrigger';
+import Modal from './Modal';
+
+export default function ColorManager({ onChange }) {
+  const colors = useContext(ColorContext);
+
+  return (
+    <Fragment>
+      <ColorPickerTrigger onClick={() => MicroModal.show('color-picker')} />
+      <Modal id="color-picker" title="Choose Chart Colors">
+        <p>You can set custom chart colors below.</p>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-sm text-white font-bold py-2 px-2 rounded w-full mt-4"
+          onClick={() => {
+            onChange(COLORS);
+            MicroModal.close('color-picker');
+          }}
+        >
+          Colorblind? Reset to the defaults
+        </button>
+
+        <div className="flex flex-col flex-auto mt-8">
+          <div className="flex">
+            <p className="text-bold w-24">Yes Votes:</p>
+            <input
+              type="color"
+              defaultValue={colors.YES}
+              onChange={(event) =>
+                onChange({ ...colors, YES: event.target.value })
+              }
+            />
+          </div>
+
+          <div className="flex mt-4">
+            <p className="text-bold w-24">No Votes:</p>
+            <input
+              type="color"
+              defaultValue={colors.NO}
+              onChange={(event) =>
+                onChange({ ...colors, NO: event.target.value })
+              }
+            />
+          </div>
+
+          <div className="flex mt-4">
+            <p className="text-bold w-24">Tie Votes:</p>
+            <input
+              type="color"
+              defaultValue={colors.NEUTRAL}
+              onChange={(event) =>
+                onChange({ ...colors, NEUTRAL: event.target.value })
+              }
+            />
+          </div>
+        </div>
+      </Modal>
+    </Fragment>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import ColorPickerTrigger from '../components/ColorPickerTrigger';
-import Modal from '../components/Modal';
+import dynamic from 'next/dynamic';
+import React, { useEffect } from 'react';
 import ColorContext, { COLORS } from '../contexts/ColorContext';
 import useLocalStorage from '../hooks/useLocalStorage';
+import ColorPickerTrigger from '../components/ColorPickerTrigger';
 import '../styles/main.css';
+
+const DynamicColorManager = dynamic(
+  () => import('../components/ColorManager'),
+  {
+    ssr: false,
+    loading: () => <ColorPickerTrigger disabled />,
+  }
+);
 
 export default function MyApp({ Component, pageProps }) {
   const [colors, setColors] = useLocalStorage('colors', COLORS);
-  const [MicroModal, setMicroModal] = useState(null);
-
-  useEffect(() => {
-    async function loadLib() {
-      const lib = await import('micromodal');
-      setMicroModal(lib.default);
-    }
-    loadLib();
-  }, []);
 
   useEffect(() => {
     let tracker = window.document.createElement('script');
@@ -30,61 +29,8 @@ export default function MyApp({ Component, pageProps }) {
 
   return (
     <ColorContext.Provider value={colors}>
-      <ColorPickerTrigger
-        disabled={!Boolean(MicroModal)}
-        onClick={() => MicroModal?.show('color-picker')}
-      />
-
+      <DynamicColorManager onChange={setColors} />
       <Component {...pageProps} />
-
-      <Modal id="color-picker" title="Choose Chart Colors">
-        <p>You can set custom chart colors below.</p>
-
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-sm text-white font-bold py-2 px-2 rounded w-full mt-4"
-          onClick={() => {
-            setColors(COLORS);
-            MicroModal?.close('color-picker');
-          }}
-        >
-          Colorblind? Reset to the defaults
-        </button>
-
-        <div className="flex flex-col flex-auto mt-8">
-          <div className="flex">
-            <p className="text-bold w-24">Yes Votes:</p>
-            <input
-              type="color"
-              defaultValue={colors.YES}
-              onChange={(event) =>
-                setColors({ ...colors, YES: event.target.value })
-              }
-            />
-          </div>
-
-          <div className="flex mt-4">
-            <p className="text-bold w-24">No Votes:</p>
-            <input
-              type="color"
-              defaultValue={colors.NO}
-              onChange={(event) =>
-                setColors({ ...colors, NO: event.target.value })
-              }
-            />
-          </div>
-
-          <div className="flex mt-4">
-            <p className="text-bold w-24">Tie Votes:</p>
-            <input
-              type="color"
-              defaultValue={colors.NEUTRAL}
-              onChange={(event) =>
-                setColors({ ...colors, NEUTRAL: event.target.value })
-              }
-            />
-          </div>
-        </div>
-      </Modal>
     </ColorContext.Provider>
   );
 }
