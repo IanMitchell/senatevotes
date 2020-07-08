9b65f0cf80ba6af6d24fa2a32c75b124d70eee25
Ian Mitchell <ian.mitchel1@live.com>
2020-07-08 11:17:35 -0700

    Add Notable Votes

--- pages/votes.js
+++ pages/votes.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Link from 'next/link';
+import Anchor from '../components/Anchor';
 import Card from '../components/Card';
 import Heading from '../components/Heading';
 import Main from '../components/Main';
 import Navigation from '../components/Navigation';
 import Social from '../components/Social';
 import { getYears, getVotesInYear } from '../lib/pages';
-import { getYearAnalysis, getPopularVotePercentage } from '../lib/votes';
+import {
+  getYearAnalysis,
+  getPopularVotePercentage,
+  getVoteYear,
+  getVoteTotals,
+  getVoteTitleAndNumber,
+  isVotePopular,
+} from '../lib/votes';
+import { getTotalPopulationVote } from '../lib/population';
 
-export default function Senate({ years }) {
+export default function Senate({ years, leaderboard }) {
   return (
     <Main>
       <Social />
@@ -44,6 +53,28 @@ export default function Senate({ years }) {
           </Link>
         ))}
       </section>
+
+      <Heading className="mt-10 border-b-2 border-black mb-4">
+        Notable Votes
+      </Heading>
+
+      <section className="flex flex-wrap -mx-2">
+        <ol className="ml-2">
+          {leaderboard.map((entry) => (
+            <li key={`${entry.year}-${entry.rollcall}`} className="p-2">
+              <Link
+                href="/votes/[year]/[vote]"
+                as={`/votes/${entry.year}/${entry.rollcall}`}
+                passHref
+              >
+                <Anchor>
+                  {entry.year} Rollcall {entry.rollcall} &bull; {entry.title}
+                </Anchor>
+              </Link>
+            </li>
+          ))}
+        </ol>
+      </section>
     </Main>
   );
 }
@@ -51,10 +82,18 @@ export default function Senate({ years }) {
 export async function getStaticProps() {
   const years = getYears();
 
+  const votesByYear = await Promise.all(
+    years.map(async (year) => getVotesInYear(year))
+  );
+
+  const populationByYear = await Promise.all(
+    years.map(async (year) => import(`../content/population/${year}.json`))
+  );
+
   const pages = await Promise.all(
-    years.map(async (year) => {
-      const population = await import(`../content/population/${year}.json`);
-      const votes = await getVotesInYear(year);
+    years.map(async (year, index) => {
+      const population = populationByYear[index];
+      const votes = votesByYear[index];
       const analysis = getYearAnalysis(votes, population);
 
       return {
@@ -65,9 +104,49 @@ export async function getStaticProps() {
     })
   );
 
+  // Get all votes
+  const leaderboard = votesByYear
+    .flat()
+    .filter((vote) => {
+      // I am a fantastic coder
+      const year = getVoteYear(vote);
+      const index = years.indexOf(year);
+      const population = populationByYear[index];
+
+      return !isVotePopular(vote, population);
+    })
+    .map((vote) => {
+      // I am a fantastic coder
+      const year = getVoteYear(vote);
+      const index = years.indexOf(year);
+      const population = populationByYear[index];
+
+      // Calculate yes percentage
+      const totals = getVoteTotals(vote);
+      const popularity = getTotalPopulationVote(vote, population);
+
+      return {
+        ...getVoteTitleAndNumber(vote),
+        year,
+        rollcall: vote.results.votes.vote.roll_call,
+        pass:
+          totals.yes /
+          (totals.yes + totals.no + totals.present + totals.not_voting),
+        popularity:
+          popularity.yes /
+          (popularity.yes + popularity.no + popularity.neutral),
+      };
+    })
+    .sort(
+      (a, b) =>
+        Math.abs(b.pass - b.popularity) - Math.abs(a.pass - a.popularity)
+    )
+    .slice(0, 10);
+
   return {
     props: {
       years: pages.sort((a, b) => b.year - a.year),
+      leaderboard,
     },
   };
 }
